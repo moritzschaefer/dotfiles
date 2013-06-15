6a50cb5dcbd09440b12a3c5acd03cacffb2d8211
Moritz Schäfer <mollitz@gmail.com>
2013-06-15 19:07:58 +0200

    improving xmobar

--- home/.xmonad/xmobar.hs
+++ home/.xmonad/xmobar.hs
@@ -1,4 +1,4 @@
--- xmobar config used by Vic Fryzel
+
 -- Author: Vic Fryzel
 -- http://github.com/vicfryzel/xmonad-config
 
@@ -18,15 +18,16 @@ Config {
        "-h", "green",
        "-l", "red"
          ] 10,
-       Run Weather "KPAO" ["-t","<tempF>F <skyCondition>","-L","64","-H","77","-n","#CEFFAC","-h","#FFB6B0","-l","#96CBFE"] 36000,
+       Run Weather "EDDB" ["-t","<tempC>°C <skyCondition>","-L","64","-H","77","-n","#CEFFAC","-h","#FFB6B0","-l","#96CBFE"] 36000,
        Run MultiCpu ["-t","Cpu: <total0> <total1> <total2> <total3>","-L","30","-H","60","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC","-w","3"] 10,
+       Run TopProc [] 50,
        Run Memory ["-t","Mem: <usedratio>%","-H","8192","-L","4096","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC"] 10,
        Run Swap ["-t","Swap: <usedratio>%","-H","1024","-L","512","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC"] 10,
-       Run Network "eth0" ["-t","Net: <rx>, <tx>","-H","200","-L","10","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC"] 10,
+       Run Network "wlan0" ["-t","Net: <rx>, <tx>","-H","200","-L","10","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC"] 10,
        Run Date "%a %b %_d %l:%M" "date" 10,
        Run StdinReader
          ],
        sepChar = "%",
        alignSep = "}{",
-       template = "%StdinReader% }{%battery%   %multicpu%   %memory%   %swap%   %eth0%   <fc=#FFFFCC>%date%</fc>   %KPAO%"
+       template = "%StdinReader% }{TopProc: %top%    %battery%   %multicpu%   %memory%   %swap%   %wlan0%   <fc=#FFFFCC>%date%</fc>   %EDDB%"
 }
