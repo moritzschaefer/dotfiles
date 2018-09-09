5c46e0ad3ad599e1afe26c5ddd56c1b440e3c33f
Moritz <mail@moritzs.de>
2018-09-09 12:57:25 +0200

    spawn firefox in correct group

--- home/.config/qtile/groups.py
+++ home/.config/qtile/groups.py
@@ -4,7 +4,7 @@ group_keys = 'nrtdgfq'
 
 groups = []
 groups.append(Group('urxvt', spawn='urxvt -e bash -c "tmux -q has-session && exec tmux attach-session -d || exec tmux new-session -n$USER -s$USER@$HOSTNAME"', exclusive=True, layout='max'))
-groups.append(Group('browser', spawn='firefox', layout='max'))
+groups.append(Group('browser', spawn='firefox', layout='max', matches=[Match(wm_class=['Navigator', 'Firefox'])]))
 groups.append(Group('chat', spawn='rambox', layout='max'))
 groups.append(Group('music', spawn='spotify', layout='max', matches=[Match(wm_class=["spotify", "Spotify"])]))
 
