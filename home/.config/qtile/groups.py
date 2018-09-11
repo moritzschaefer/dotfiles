9f58ff3c55c3c32737335f6ad4413a2ef0a9dfdf
Moritz <mail@moritzs.de>
2018-09-11 09:42:15 +0200

    tweak font size

--- home/.config/qtile/groups.py
+++ home/.config/qtile/groups.py
@@ -5,9 +5,8 @@ group_keys = 'nrtdgfq'
 groups = []
 groups.append(Group('urxvt', spawn='urxvt -e bash -c "tmux -q has-session && exec tmux attach-session -d || exec tmux new-session -n$USER -s$USER@$HOSTNAME"', layout='max'))
 groups.append(Group('browser', spawn='firefox', layout='max', matches=[Match(wm_class=['Navigator', 'Firefox'])]))
-groups.append(Group('chat', spawn='rambox', layout='max'))
+groups.append(Group('chat', spawn='rambox', layout='max', matches=[Match(wm_class=['rambox', 'Rambox'])]))
 groups.append(Group('music', spawn='spotify', layout='max', matches=[Match(wm_class=["spotify", "Spotify"])]))
-
 
 for i in range(len(group_keys)-len(groups)):
     groups.append(Group(f'misc{i+1}'))
