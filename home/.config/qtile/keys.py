a8299573ffeb82628c1f28383906ce71d68b53cd
Moritz <mail@moritzs.de>
2018-09-22 02:02:52 +0200

    use proper alt and cmd keys

--- home/.config/qtile/keys.py
+++ home/.config/qtile/keys.py
@@ -8,8 +8,8 @@ from libqtile.command import lazy
 
 from groups import groups, group_keys
 
-mod = 'mod1'  # use alt for supermod key
-alt = 'mod4'  # use supermodkey for alt...
+alt = 'mod1'
+mod = 'mod4'
 
 music_cmd = ('dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify '
              '/org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.')
